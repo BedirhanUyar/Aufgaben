d08bd0e343267b8907e1b24bec0fbb3d0753b2d0
Bedirhan Uyar <you@example.com>
2022-07-05 13:21:53 +0200

    1

--- src/Aufgabe5.java
+++ src/Aufgabe5.java
@@ -3,23 +3,21 @@ import java.io.*;
 public class Aufgabe5 {
 
 
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-            try (BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"))) {
-                StringBuilder sb = new StringBuilder();
-                String line = br.readLine();
+        try (BufferedReader br = new BufferedReader(new FileReader("src/sample.txt"))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-                while (line != null && !line.contains("#")) {
+            while (line != null){
+                if (!Character.isWhitespace(line.charAt(0)) && !line.startsWith("#")){
                     sb.append(line);
                     sb.append(System.lineSeparator());
-                    line = br.readLine();
                 }
-                String everything = sb.toString();
-                System.out.println(everything);
+                    line = br.readLine();
             }
-
+            String everything = sb.toString();
+            System.out.println(everything);
         }
-
-
-
+    }
 }
